7c4322445ae1bbaca18816bdcee4e0245ef0222e
Zach Armstrong <64998135+zarmstrong10@users.noreply.github.com>
2020-07-15 18:13:26 -0500

    Revised Zach queries 33 and 48
    Updated the 33 query

--- omdb_sql_queries.sql
+++ omdb_sql_queries.sql
@@ -20,11 +20,11 @@ SELECT mv.movie_id,
        mvdt.country,
        mvdt.genre,
        mvdt.plot,
-       Count(mvtv.trivia)         AS 'Count movie trivia',
-       Count(mvkw.keyword)        AS 'Count movie keyword',
-       Count(mvmd.movie_media_id) AS 'Count movie media',
-       Count(mvsg.song_id)        AS 'Count soungs',
-       Count(ppl.id)              AS 'Count people'
+       Count(DISTINCT mvtv.trivia)         AS 'Count movie trivia',
+       Count(DISTINCT mvkw.keyword)        AS 'Count movie keyword',
+       Count(DISTINCT mvmd.movie_media_id) AS 'Count movie media',
+       Count(DISTINCT mvsg.song_id)        AS 'Count soungs',
+       Count(DISTINCT ppl.id)              AS 'Count people'
 FROM   movies mv
        LEFT JOIN movie_data mvdt
               ON mvdt.movie_id = mv.movie_id
@@ -41,4 +41,5 @@ FROM   movies mv
        LEFT JOIN movie_people mvp
               ON mvp.movie_id = mv.movie_id
        LEFT JOIN people ppl
-              ON ppl.id = mvp.people_id  
+              ON ppl.id = mvp.people_id
+GROUP  BY mv.movie_id  
